6886a6f3aa2c22f833e27c91a5b09f51561b01fa
radiylon <radiylon@gmail.com>
2025-08-11 11:45:54 -0700

    Update README and config

--- config/index.ts
+++ config/index.ts
@@ -1,5 +1,6 @@
 export const config = {
   urls: {
-    api: "https://yw32vir1i0.execute-api.us-west-1.amazonaws.com"
+    api: "https://4kfuyx9e1m.execute-api.us-west-1.amazonaws.com"
+    // api: "https://yw32vir1i0.execute-api.us-west-1.amazonaws.com"
   }
 }
