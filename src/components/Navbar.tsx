96885dd5c81ec4f02df265d41d127973939de7b0
radiylon <radiylon@gmail.com>
2025-08-12 22:20:58 -0700

    Add Loading component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Navbar({ caregiverData }: { caregiverData?: Caregiver }) {
   return (
-    <div className="navbar bg-base-100 shadow-sm border-b-2 border-secondary">
+    <div className="navbar bg-base-100 shadow-sm border-b-2 border-primary">
       <div className="flex-1 flex px-8 justify-between">
         <Link href="/" className="text-2xl font-bold text-base-content">MEDTHRIVE</Link>
         {caregiverData?.first_name && caregiverData?.last_name && (
--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,12 +1,15 @@
 import Navbar from "@/components/Navbar";
 import useCaregivers from "@/hooks/useCaregivers";
+import Loading from "@/components/Loading";
 
 // Hardcoded caregiver_id to represent the Caregiver
 const CAREGIVER_ID = "97bca0dd-d50e-4b69-b416-21df0421dc15";
 
 export default function DefaultLayout({ children }: { children: React.ReactNode }) {
   const { useGetCaregiverById } = useCaregivers();
-  const { data: caregiverData } = useGetCaregiverById(CAREGIVER_ID);
+  const { data: caregiverData, isLoading } = useGetCaregiverById(CAREGIVER_ID);
+
+  if (isLoading) return <Loading />;
 
   return (
     <div className="flex flex-col bg-base-200 min-h-screen">
