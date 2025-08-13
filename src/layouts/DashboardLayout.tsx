4723c18ed95f3046bb11fce0e35900c62fa79461
radiylon <radiylon@gmail.com>
2025-08-12 20:55:35 -0700

    Get daisyUI theme working

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,6 +1,6 @@
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex flex-col gap-4 m-4">
+    <div className="flex flex-col gap-4 m-4 bg-base-100">
       <h1 className="text-2xl font-bold text-primary">Medthrive</h1>
       <h1 className="text-2xl font-bold text-primary">Caregiver Dashboard</h1>
       {children}
